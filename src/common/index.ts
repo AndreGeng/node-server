61dbc69885ecbeb5a6b9e45820c5e416e7b3d9e0
山丘 <gengze@innotechx.com>
2020-03-30 21:29:16 +0800

    feat: bug fix

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,9 @@
 /** node-server-eject mysql -- start */
 import db from "./mysql"
 /** node-server-eject mysql -- end */
+/** node-server-eject redis -- start */
 import redis from "./redis"
+/** node-server-eject redis -- end */
 
 export * from "./constant"
 export * from "./helpers"
